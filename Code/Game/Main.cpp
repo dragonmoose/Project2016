9f5e55683bad8e6b6f171f43542bd9798aeb6cea
dragonmoose <eholmlid@gmail.com>
2015-12-03 00:32:47 +0100

    removed test code

--- Code/Game/Main.cpp
+++ Code/Game/Main.cpp
@@ -4,50 +4,13 @@
 #include "TestModule2.h"
 #include <iostream>
 #include <map>
-
-namespace CmdMan
-{
-	std::map<std::string, std::function<void()>> m_Map;
-	void Register(const std::string& p_Cmd, std::function<void()> func)
-	{
-		m_Map[p_Cmd] = func;
-	}
-	
-
-	void Invoke(const std::string& p_Cmd)
-	{
-		m_Map[p_Cmd]();
-	}
-};
-
-void TestFunc()
-{
-	std::cout << "TestFunc\n";
-}
-
-void TestFunc2()
-{
-	std::cout << "TestFunc2!\n";
-}
+#include <boost/lexical_cast.hpp>
 
 int WINAPI WinMain(HINSTANCE p_hInstance, HINSTANCE p_hPrevInstance, LPSTR p_lpCmdLine, int p_nCmdShow)
 {
-	Hawk::Core l_Core;
-	CmdMan::Register("cmd.test", &TestFunc);
-	CmdMan::Register("cmd.test2", &TestFunc2);
-
-	CmdMan::Invoke("cmd.test2");
-	while (1);
-
-
-
-
-
-
-
 	try
 	{
-		
+		Hawk::Core l_Core;
 		l_Core.OpenWindow(p_hInstance, "TestApp");
 		for (int i = 0; i < 100; i++)
 		{
