a91b754d41a824fc1e8d74d421cbdde4ecc70f50
dragonmoose <eholmlid@gmail.com>
2015-12-30 23:50:35 +0100

    assert

---
+++ Code/Hawk/System/Assert.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "Console/Logger.h"
+#include <intrin.h>
+
+#ifdef HAWK_DEBUG
+	#define ASSERT(p, msg)							\
+		if ((p))									\
+		{											\
+		}											\
+		else										\
+		{	LOG(msg, "assert", Fatal);				\
+			__debugbreak();							\
+		}
+#else
+	#define ASSERT(p, msg)
+#endif
