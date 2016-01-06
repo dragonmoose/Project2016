5a525b22004031b78883e1aec92648bdd845db4f
dragonmoose <eholmlid@gmail.com>
2016-01-06 23:44:24 +0100

    use asserts

--- Code/Hawk/Debug/Profiler.cpp
+++ Code/Hawk/Debug/Profiler.cpp
@@ -1,6 +1,7 @@
 #include "pch.h"
 #include "Profiler.h"
 #include "ProfilerManager.h"
+#include "Debug/Assert.h"
 #include "System/Duration.h"
 
 namespace Hawk {
@@ -24,30 +25,18 @@ Profiler::~Profiler()
 void Profiler::Start()
 {
 #ifdef HAWK_DEBUG
-	if (!m_bRunning)
-	{
-		m_StartTime.SetToNow();
-		m_bRunning = true;
-	}
-	else
-	{
-		LOG("Attempting to start an already running profiler. Name: " << m_Name, "profiler", Warning);
-	}
+	ASSERT(!m_bRunning, "Attempting to start an already running profiler.Name: " << m_Name);
+	m_StartTime.SetToNow();
+	m_bRunning = true;
 #endif
 }
 
 void Profiler::Stop()
 {
 #ifdef HAWK_DEBUG
-	if (m_bRunning)
-	{
-		ProfilerManager::Add(m_Name, Time::Now() - m_StartTime);
-		m_bRunning = false;
-	}
-	else
-	{
-		LOG("Stopping profiler that was never started, or calling Stop() more than once. Name: " << m_Name, "profiler", Warning);
-	}
+	ASSERT(m_bRunning, "Attempting to stop profiler that was never started, or calling Stop() more than once. Name: " << m_Name);
+	ProfilerManager::Add(m_Name, Time::Now() - m_StartTime);
+	m_bRunning = false;
 #endif
 }
 }
