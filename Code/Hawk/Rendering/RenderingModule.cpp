3063a7f835989974285fe26c4bf8b16c34a507ed
dragonmoose <eholmlid@gmail.com>
2016-01-12 23:37:10 +0100

    included d3dx12.h + added debug layer init

--- Code/Hawk/Rendering/RenderingModule.cpp
+++ Code/Hawk/Rendering/RenderingModule.cpp
@@ -1,11 +1,18 @@
 #include "pch.h"
 #include "RenderingModule.h"
+#include <client.h>
+#include <d3d12.h>
 
 namespace Hawk {
 namespace Gfx {
 
 void RenderingModule::Initialize()
 {
+#ifdef HAWK_DEBUG
+	ComPtr<ID3D12Debug> l_DebugInterface;
+	THROW_IF_COMERR(D3D12GetDebugInterface(IID_PPV_ARGS(&l_DebugInterface)), "Failed to get d3d debug interface");
+	l_DebugInterface->EnableDebugLayer();
+#endif
 }
 
 void RenderingModule::RegisterEvents(EventManager& p_EventManager)
