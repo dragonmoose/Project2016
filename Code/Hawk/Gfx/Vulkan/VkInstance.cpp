1334aa2d92e509b1b63bb94d776fde99a0c69004
dragonmoose <eholmlid@gmail.com>
2016-03-02 10:43:22 +0100

    vkinstance

--- Code/Hawk/Gfx/Vulkan/VkInstance.cpp
+++ Code/Hawk/Gfx/Vulkan/VkInstance.cpp
@@ -9,7 +9,11 @@ VkInstance::VkInstance()
 	VkApplicationInfo l_AppInfo = {};
 	l_AppInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
 	l_AppInfo.pNext = nullptr; // must be null or pointer to an extension-specific structure
-	l_AppInfo.pApplicationName = "HawkVulkanRenderer";
+	l_AppInfo.pApplicationName = "";
+	l_AppInfo.applicationVersion = 0;
+	l_AppInfo.pEngineName = Constants::c_EngineInfo.GetName().c_str();
+	l_AppInfo.engineVersion = Constants::c_EngineInfo.GetVersionID();
+	l_AppInfo.apiVersion = VK_API_VERSION;
 }
 
 VkInstance::~VkInstance()
