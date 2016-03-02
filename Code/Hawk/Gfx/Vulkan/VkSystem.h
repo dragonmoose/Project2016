c9ebc354aadee4cd431f4e7fbbabe5280d62d740
dragonmoose <eholmlid@gmail.com>
2016-03-02 19:48:45 +0100

    VK_THROW

--- Code/Hawk/Gfx/Vulkan/VkSystem.h
+++ Code/Hawk/Gfx/Vulkan/VkSystem.h
@@ -1,6 +1,8 @@
 #pragma once
-#include <vulkan/vulkan.h>
+#include "VkUtil.h"
 #include "System/Exception.h"
+#include <vulkan/vulkan.h>
 
-#define VK_THROW_IF_ERR(p, msg)	THROW_IF(p < 0, msg)
-#define VK_THROW_IF_NOT_SUCCESS(p, msg) THROW_IF_NOT(p == 0, msg)
+#define VK_THROW(p, msg) THROW(msg << " [VkResult: " << Hawk::Gfx::VkUtil::ResultToString(p) << "]")
+#define VK_THROW_IF_ERR(p, msg)	if ((p < 0)) VK_THROW(p, msg)
+#define VK_THROW_IF_NOT_SUCCESS(p, msg) if ((p != 0)) VK_THROW(p, msg)
