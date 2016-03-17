280f11d065780c37a798b5063d053eca7a241d15
dragonmoose <eholmlid@gmail.com>
2016-03-17 17:32:50 +0100

    added commandpool

---
+++ Code/Hawk/Gfx/Vulkan/CommandPool.cpp
@@ -0,0 +1,2 @@
+#include "pch.h"
+#include "CommandPool.h"
---
+++ Code/Hawk/Gfx/Vulkan/CommandPool.h
@@ -0,0 +1,15 @@
+namespace Hawk {
+namespace Gfx {
+
+class CommandPool final
+{
+public:
+	CommandPool();
+	CommandPool(const CommandPool&) = delete;
+	CommandPool& operator=(const CommandPool&) = delete;
+
+private:
+};
+
+}
+}
