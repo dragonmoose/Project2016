cba6eb9f0da1195f6e98bcce64e564bd5d41a9f3
dragonmoose <eholmlid@gmail.com>
2016-04-17 23:36:19 +0200

    vsync mode

--- Code/Hawk/Gfx/Vulkan/Constants.cpp
+++ Code/Hawk/Gfx/Vulkan/Constants.cpp
@@ -8,7 +8,7 @@ namespace Vulkan {
 const uint32 Constants::c_uiNumBackBuffers = 2;
 const VkFormat Constants::c_PreferredBackBufferColorFormat = VK_FORMAT_B8G8R8A8_UNORM;
 const VkFormat Constants::c_PreferredDepthBufferFormat = VK_FORMAT_D32_SFLOAT_S8_UINT;
-const VkPresentModeKHR Constants::c_PresentationMode = VK_PRESENT_MODE_FIFO_RELAXED_KHR;
+const VkPresentModeKHR Constants::c_PresentationMode = VK_PRESENT_MODE_FIFO_KHR;
 const VkColorSpaceKHR Constants::c_BackBufferColorSpace = VK_COLORSPACE_SRGB_NONLINEAR_KHR;
 
 }
